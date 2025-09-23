524ff4438606bf0971a78f6ff3aaf88bd5cef2f8
Caleb Norton <n0603919@outlook.com>
2025-09-23 16:57:37 -0500

    internal events

---
+++ priv/repo/migrations/20250923212624_create_events.exs
@@ -0,0 +1,17 @@
+defmodule IeeeTamuPortal.Repo.Migrations.CreateEvents do
+  use Ecto.Migration
+
+  def change do
+    create table(:events, primary_key: false) do
+      add :uid, :uuid, primary_key: true
+      add :dtstart, :utc_datetime, null: false
+      add :dtend, :utc_datetime
+      add :summary, :string, null: false
+      add :description, :text
+      add :location, :string
+      add :organizer, :string
+
+      timestamps(type: :utc_datetime)
+    end
+  end
+end
