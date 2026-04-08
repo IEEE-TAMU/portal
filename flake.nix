13a7b4bbe06eac562f42738f1bb8dfd82ba0bd27
Caleb Norton <n0603919@outlook.com>
2026-04-08 16:10:01 -0500

    release: v0.2.14

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
             inherit mixNixDeps;
             pname = "ieee-tamu-portal";
             src = ./.;
-            version = "0.2.13";
+            version = "0.2.14";
 
             stripDebug = true;
 
