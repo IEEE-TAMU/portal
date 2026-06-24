cdeb5e89ae3d974ff25f2e7cf2c4b2eec17013c0
Caleb Norton <n0603919@outlook.com>
2026-06-24 16:39:40 -0500

    release: v0.2.19

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule IeeeTamuPortal.MixProject do
   def project do
     [
       app: :ieee_tamu_portal,
-      version: "0.2.18",
+      version: "0.2.19",
       elixir: "1.20.1",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
